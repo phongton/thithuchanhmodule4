f9a945509dfd8316b84c8d7fe72dfac69ec70571
phongton <phongton219@gmail.com>
2024-08-12 19:54:04 +0700

    test

---
+++ src/main/java/com/codegym/thi_thuc_hanh_module4/ThiThucHanhModule4Application.java
@@ -0,0 +1,13 @@
+package com.codegym.thi_thuc_hanh_module4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiThucHanhModule4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThiThucHanhModule4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/thi_thuc_hanh_module4/ThiThucHanhModule4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.thi_thuc_hanh_module4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiThucHanhModule4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
